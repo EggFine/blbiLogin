b062eb0f380e803f7bed7b74c6fbb659bd092f20
EggFine <1079742633@qq.com>
2024-07-20 16:32:46 +0800

    delete src\main\java\com\blbilink\blbilogin\modules\events\Folia.java

--- src/main/java/com/blbilink/blbilogin/modules/events/Folia.java
+++
@@ -1,7 +0,0 @@
-package com.blbilink.blbilogin.modules.events;
-
-import io.papermc.paper.threadedregions.scheduler.RegionScheduler;
-
-public class Folia {
-    public RegionScheduler regionScheduler;
-}
